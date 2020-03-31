76c79e0c7932365da15dcca4405ca9e0d0eca39d
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-31 15:09:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,10 +31,10 @@ class Song
       if genre_hash.include?(genre) == true
         genre_hash[genre] += 1
       else
-        genre_hash[genre] = num
+        genre_hash[genre] = 1
       end
     end
-
+    genre_hash
   end
 
 end
