e04137c037d991fb4910e16874d6e48967b40ee5
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-31 14:55:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,19 @@ class Song
   @@count = 0
   @@genres = []
 
-  def initialize
+  def initialize(song_name, artist, genre)
+    @name = song_name
+    @artist = artist
+    @genre = genre
     @@count += 1
+    @@genres << genre
+    @@artists << artist
   end
+
   def self.count
     @@count
   end
+
   def self.genres
     @@genres.map do |genre|
       if @@genres.include?(genre) == false
@@ -16,5 +23,5 @@ class Song
       end
     end
   end
-  
+
 end
